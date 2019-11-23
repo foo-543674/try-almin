d7c52da497a85bc147891513ee9e2b71bc4d4e3f
foo-543674 <foobar78563@gmail.com>
2019-11-23 15:56:36 +0900

    chore: install auto routing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <v-app>
     <v-content>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-export default Vue.extend({
-  name: "App",
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  data: () => ({
-    //
-  })
-});
-</script>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
